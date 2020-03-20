76dfc03ea956decb961adb427a5941f62f878add
aniksharif <aniksharif@gmail.com>
2020-03-21 01:19:23 +0600

    data dupliction fixed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 8080;
 const request = require('request');
 const cheerio = require("cheerio");
+const R = require('ramda');
 
 app.get('/allinfo', (req, res) =>
 
@@ -12,14 +13,9 @@ app.get('/allinfo', (req, res) =>
 
         }
 
-        var country = [];
-        var $ = cheerio.load(body);
-        const tempCases = $('div#maincounter-wrap').eq(0).text().split(':')[1].trim();
-        const cases = parseInt(tempCases.replace(/,/g, ''), 10);
-        const tempDeaths = $('div#maincounter-wrap').eq(1).text().split(':')[1].trim();
-        const deaths = parseInt(tempDeaths.replace(/,/g, ''), 10);
-        const tempRecovered = $('div#maincounter-wrap').eq(2).text().split(':')[1].trim();
-        const recovered = parseInt(tempRecovered.replace(/,/g, ''), 10);
+        let country = [];
+        let $ = cheerio.load(body);
+        
         $('tbody>tr').each(function (index, element) {
 
 
@@ -36,7 +32,13 @@ app.get('/allinfo', (req, res) =>
 
         });
 
-        res.json(country);
+function removeDuplicates(myArr, prop) {
+    return myArr.filter((obj, pos, arr) => {
+        return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+    });
+}
+
+        res.json(removeDuplicates(country,'country'));
     })
 
 
@@ -49,8 +51,8 @@ app.get('/globalinfo', (req, res) =>
             alert(response.statusCode)
         }
 
-        var country = [];
-        var $ = cheerio.load(body);
+        let country_list = [];
+        let $ = cheerio.load(body);
         const tempCases = $('div#maincounter-wrap').eq(0).text().split(':')[1].trim();
         const cases = parseInt(tempCases.replace(/,/g, ''), 10);
         const tempDeaths = $('div#maincounter-wrap').eq(1).text().split(':')[1].trim();
@@ -61,12 +63,12 @@ app.get('/globalinfo', (req, res) =>
         $('tbody>tr').each(function (index, element) {
 
 
-            country[index] = {};
-            country[index]= $(element).find('td:nth-child(1)').text().trim();
+            country_list[index] = {};
+            country_list[index]= $(element).find('td:nth-child(1)').text().trim();
 
         });
 
-        res.json({ cases: cases, deaths: deaths, recovered: recovered,country:country});
+        res.json({ cases: cases, deaths: deaths, recovered: recovered,country:[...new Set(country_list)]});
     })
 
 
